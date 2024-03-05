77121ced319f80e2c145a0af5ef86738291a778d
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-05 15:28:20 +1100

    setup bext-intl

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ['en-US', 'fr'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en-US',
-  },
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const createNextIntlPlugin = require('next-intl/plugin');
 
-module.exports = nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {};
+
+module.exports = withNextIntl(nextConfig);
