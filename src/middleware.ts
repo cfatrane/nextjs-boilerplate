857f16cd2f8510fd0680ca0f8559bff7e84030af
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-05 15:37:33 +1100

    remove useless file and remove dl in lang

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'fr', 'de'],
+  locales: ['en', 'fr'],
 
   // Used when no locale matches
   defaultLocale: 'en',
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|fr|en)/:path*'],
+  matcher: ['/', '/(fr|en)/:path*'],
 };
