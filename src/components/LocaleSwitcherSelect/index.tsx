635b155116fc87feecd42bad79f50ba19cb1390e
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-05 17:25:06 +1100

    change select lang

--- src/components/LocaleSwitcherSelect/index.tsx
+++ src/components/LocaleSwitcherSelect/index.tsx
@@ -21,18 +21,14 @@ export default function LocaleSwitcherSelect({
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
-  const params = useParams();
+  // const params = useParams();
+
+  // TODO: Fix change language
 
   function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
     const nextLocale = event.target.value;
     startTransition(() => {
-      router.replace(
-        // @ts-expect-error -- TypeScript will validate that only known `params`
-        // are used in combination with a given `pathname`. Since the two will
-        // always match for the current route, we can skip runtime checks.
-        { pathname, params },
-        { locale: nextLocale },
-      );
+      router.replace(pathname, { locale: nextLocale });
     });
   }
 
