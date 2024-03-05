4f69306e02a0c51434ad1c97622fb1a4da4d6a42
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-05 15:08:54 +1100

    srart

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US', 'fr'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
