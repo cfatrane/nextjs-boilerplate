28e73764d43f14a1f33d5dbb1b0b4d771097014e
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-07 18:03:42 +1100

    testing pre-commit code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,8 @@ export default function Index() {
 
 
 
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
