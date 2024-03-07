d9dee92a4305673d4805520e58a7a3b484847330
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-07 18:11:59 +1100

    testing pre-commit code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,10 +6,6 @@ import LocaleSwitcher from '@/components/LocaleSwitcher';
 export default function Index() {
   const t = useTranslations('Home');
 
-
-
-
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
