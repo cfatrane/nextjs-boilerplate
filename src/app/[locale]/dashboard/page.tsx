cac6be6d322d7fba1ad233a152941e0449b3abf8
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-06-07 21:54:47 +0200

    add new pages

---
+++ src/app/[locale]/dashboard/page.tsx
@@ -0,0 +1,11 @@
+function DashboardPage() {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex min-h-screen items-center justify-center">
+        <p className="text-8xl font-bold">Dashboard Page</p>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardPage;
