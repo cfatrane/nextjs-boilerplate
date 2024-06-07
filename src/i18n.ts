b7eae7c4e695df0461d5c5791243d1367dd3c69f
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-06-07 20:22:38 +0200

    add not found to params

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,6 +13,7 @@ export default getRequestConfig(async ({ locale }) => {
     messages: {
       ...(await import(`../messages/${locale}/home.json`)).default,
       ...(await import(`../messages/${locale}/auth.json`)).default,
+      ...(await import(`../messages/${locale}/not-found.json`)).default,
     },
   };
 });
