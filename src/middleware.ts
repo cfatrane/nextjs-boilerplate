52c1fbd60462528903c810443f48d55925188a31
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-06-07 22:48:59 +0200

    update the middleware to work wih locale in path

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,35 +4,52 @@ import createIntlMiddleware from "next-intl/middleware";
 
 import { auth } from "@/auth";
 
+import { HEADER_ITEMS } from "@/routes/protectedRoutes";
+
 // * Link : https://next-intl-docs.vercel.app/docs/routing/middleware#example-auth-js
 
 // 1. Specify protected and public routes
-const protectedRoutes = ["/", "/user"]; // Routes that require authentication
+const protectedRoutes = HEADER_ITEMS.map((item) => item.href); // Routes that require authentication
 const publicRoutes = ["/signin", "/signup", "/forgot-password"]; // Routes that don't require authentication
 const baseRoute = "/dashboard";
 
-// Create i18n middleware
-const i18nMiddleware = createIntlMiddleware({
-  locales: ["en", "fr", "de"],
+const locales = ["en", "fr"];
+
+const pathNameRegex = (pages: string[]): RegExp => {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join("|")}))?(${pages
+      .flatMap((p) => (p === "/" ? ["", "/"] : p))
+      .join("|")})/?$`,
+    "i",
+  );
+
+  return publicPathnameRegex;
+};
+
+const intlMiddleware = createIntlMiddleware({
+  locales,
   localePrefix: "as-needed",
   defaultLocale: "en",
 });
 
 // Combined middleware function
 export default auth(async (req) => {
-  const path = req.nextUrl.pathname;
+  const { pathname } = req.nextUrl;
 
   // Apply i18n middleware
-  const response = i18nMiddleware(req);
+  const response = intlMiddleware(req);
 
   // Check if the current route is protected or public
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
+  const protectedPathnameRegex = pathNameRegex(protectedRoutes);
+  const publicPathnameRegex = pathNameRegex(publicRoutes);
+
+  const isProtectedRoute = protectedPathnameRegex.test(pathname);
+  const isPublicRoute = publicPathnameRegex.test(pathname);
 
   const isLoggedIn = !!req.auth;
 
   // console.log(
-  //   `Redirecting: isLoggedIn=${isLoggedIn}, isProtectedRoute=${isProtectedRoute}, isPublicRoute=${isPublicRoute}, path=${path}`
+  //   `Redirecting: isLoggedIn=${isLoggedIn}, isProtectedRoute=${isProtectedRoute}, isPublicRoute=${isPublicRoute}, path=${pathname}`,
   // );
 
   // Redirect to /signin if the user is not authenticated
@@ -49,7 +66,7 @@ export default auth(async (req) => {
     return NextResponse.redirect(new URL(baseRoute, req.nextUrl));
   }
 
-  if (path === "/" && isLoggedIn) {
+  if (pathname === "/" && isLoggedIn) {
     return NextResponse.redirect(new URL(baseRoute, req.nextUrl));
   }
 
@@ -58,10 +75,5 @@ export default auth(async (req) => {
 
 // Combined config
 export const config = {
-  matcher: [
-    "/",
-    "/((?!api|_next|.*\\..*).*)",
-    // "/(de|fr|en)/:path*",
-    // "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)",
-  ],
+  matcher: ["/", "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
 };
