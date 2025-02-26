31205634bad1efb33822291e520bb9520ff88558
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2025-02-26 14:40:00 +0100

    feat: add notFound to messages

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,13 +1,13 @@
 import { getRequestConfig } from "next-intl/server";
 
-import { routing } from "./routing";
+import { Locale, routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
   // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (!locale || !routing.locales.includes(locale as Locale)) {
     locale = routing.defaultLocale;
   }
 
@@ -16,7 +16,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
     messages: {
       ...(await import(`../../messages/${locale}/home.json`)).default,
       ...(await import(`../../messages/${locale}/auth.json`)).default,
-      ...(await import(`../../messages/${locale}/not-found.json`)).default,
+      ...(await import(`../../messages/${locale}/notFound.json`)).default,
     },
   };
 });
