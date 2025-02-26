e20793b0be9b3be0faea4ef278b3cb3b34dd5f5a
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2025-02-26 15:41:09 +0100

    fix: export default for prisma lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,4 +10,6 @@ declare const globalThis: {
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
+export default prisma;
+
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
