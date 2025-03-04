ce351b628cac6ad5010b2e2e47b138de6a415516
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2025-03-04 20:16:47 +0100

    feat: create webhooks to sync user in db with clerk

---
+++ src/app/[locale]/api/webhooks/user.ts
@@ -0,0 +1,56 @@
+import { DeletedObjectJSON, UserJSON } from "@clerk/nextjs/server";
+import dayjs from "dayjs";
+
+import { createUser, deleteUser, updateUser } from "@/db/user";
+
+export const syncCreatedUser = async (user: UserJSON) => {
+  if (user) {
+    const clerkUserId = user.id;
+
+    await createUser({
+      clerkUserId,
+      createdAt: dayjs(user.created_at).toDate() ?? "",
+      email: user.email_addresses[0]?.email_address ?? "",
+      firstName: user.first_name ?? "",
+      hasVerifiedEmailAddress:
+        user.email_addresses[0]?.verification?.status === "verified",
+      imageUrl: user.image_url ?? "",
+      lastName: user?.last_name ?? "",
+      lastSignInAt: user.last_sign_in_at
+        ? dayjs(user.last_sign_in_at).toDate()
+        : dayjs().toDate(),
+      updatedAt: dayjs(user.updated_at).toDate() ?? "",
+      username: user?.username ?? "",
+    });
+  }
+};
+
+export const syncUpdatedUser = async (user: UserJSON) => {
+  if (user) {
+    const clerkUserId = user.id;
+
+    await updateUser({
+      clerkUserId,
+      data: {
+        createdAt: dayjs(user.created_at).toDate() ?? "",
+        email: user.email_addresses[0]?.email_address ?? "",
+        firstName: user.first_name ?? "",
+        hasVerifiedEmailAddress:
+          user.email_addresses[0]?.verification?.status === "verified",
+        imageUrl: user.image_url ?? "",
+        lastName: user?.last_name ?? "",
+        lastSignInAt: dayjs(user.last_sign_in_at).toDate() ?? "",
+        updatedAt: dayjs(user.updated_at).toDate() ?? "",
+        username: user?.username ?? "",
+      },
+    });
+  }
+};
+
+export const syncDeletedUser = async (user: DeletedObjectJSON) => {
+  if (user) {
+    const clerkUserId = user.id;
+
+    await deleteUser(clerkUserId!);
+  }
+};
